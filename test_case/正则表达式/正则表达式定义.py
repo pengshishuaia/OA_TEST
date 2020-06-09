7843b5010c3576539110ea952ec862fe7f4547f6
pengshishuai <your email381833817@qq.com>
2020-06-09 17:28:00 +0800

    python知识点

---
+++ test_case/正则表达式/正则表达式定义.py
@@ -0,0 +1,35 @@
+'''
+分组：()
+re 模块
+match
+search
+findall
+sub(正则表单时，新内容，str字符串)  替换
+'''
+import re
+#
+# msg = '0012-85068083'
+# result = re.match(r'(\d{3}|\d{4})-(\d{8})$',msg)
+# print(result)
+# print(result.group(1))
+# print(result.group(2))
+#
+#
+# msg1 = '<h1>hello word<h1>'
+# result = re.match(r'(<\w+>)(.+)(<\w+>)$',msg1)
+# print(result)
+# print(result.group(1))
+# print(result.group(2))
+# print(result.group(3))
+#
+msg2 = '<h1>hello word1<h1>'
+result1 = re.match(r'<(\w+)>(.+)<\1>',msg2)
+print(result1)
+print(result1.group(1))
+
+
+
+msg3 = '<html><h1>helloword<h1><html>'
+
+result = re.match(r'<(?P<name>\w+)><(?P<name1>\w+)>(.+)<(?P=name1)><(?P=name)>',msg3)
+print(result)
